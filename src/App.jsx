d10aece9d6f2c3c512697a247cd8d1a478dc543c
Hasan <thehasandev@gmail.com>
2023-11-20 20:38:09 +0600

    add-input

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className='flex justify-center items-center w-full h-screen bg-blue-600'>
       <div className='w-96 h-96 bg-white '>
-
+        <div className='w-full'>
+          <input type="text" className='w-full border-[1px] border-black/30 py-4 px-2'/>
+        </div>
       </div>
     </div>
   )
